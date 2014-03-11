9c781a4e1f865862a4d81f015abafac9184a1025
Alexandr <alexandru.agp@gmail>
2014-03-11 23:42:31 +0200

    finished lab 2

--- WP/Lab#2/WP2/WP2/winmain.cpp
+++ WP/Lab#2/WP2/WP2/winmain.cpp
@@ -41,7 +41,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 	hWnd = CreateWindow(szAppName, TEXT("Acid Trip"), 
 						WS_OVERLAPPEDWINDOW | WS_VSCROLL | WS_HSCROLL | WS_SYSMENU,
-						600, 400, 640, 200, NULL, NULL, hInstance, NULL);
+						600, 400, 500, 450, NULL, NULL, hInstance, NULL);
 
 	RegisterHotKey(hWnd, HK_CLOSE, MOD_CONTROL, 0x58);// Ctrl + X
 	RegisterHotKey(hWnd, HK_BGCOLOR, MOD_ALT, 0x43);// Alt + C
@@ -101,26 +101,26 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		sbInfo.fMask = SIF_RANGE|SIF_POS;
 		sbInfo.nMin = 0;
 		sbInfo.nMax = 255;
-		sbInfo.nPos = 255;
+		sbInfo.nPos = 0;
 
 		SetScrollInfo(hScrollStatic, SB_CTL, &sbInfo, TRUE);
 
 		hScrollHeight = CreateWindow(L"SCROLLBAR", NULL, 
-									 WS_CHILD | WS_VISIBLE| SBS_HORZ | SBS_TOPALIGN,
-									 10, 90, 200, 20, hWnd, (HMENU)ID_SCROLLHEIGHT, NULL, NULL);
+									 WS_CHILD | WS_VISIBLE| SBS_VERT | SBS_TOPALIGN,
+									 10, 120, 20, 200, hWnd, (HMENU)ID_SCROLLHEIGHT, NULL, NULL);
 
 		sbInfo.cbSize = sizeof(SCROLLINFO);
 		sbInfo.fMask = SIF_RANGE|SIF_POS;
 		sbInfo.nMin = 0;
 		sbInfo.nMax = 1000;
-		sbInfo.nPos = 240;
+		sbInfo.nPos = 450;
 
 		SetScrollInfo(hScrollHeight, SB_CTL, &sbInfo, TRUE);
 
 
 		hScrollWidth = CreateWindow(L"SCROLLBAR", NULL, 
 									 WS_CHILD | WS_VISIBLE | SBS_HORZ | SBS_TOPALIGN,
-									 10, 120, 200, 20, hWnd, (HMENU)ID_SCROLLWIDTH, NULL, NULL);
+									 10, 90, 200, 20, hWnd, (HMENU)ID_SCROLLWIDTH, NULL, NULL);
 
 		sbInfo.cbSize = sizeof(SCROLLINFO);
 		sbInfo.fMask = SIF_RANGE|SIF_POS;
@@ -140,29 +140,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		}
 		break;
 	
-	case WM_GETMINMAXINFO:
-		{
-			LPMINMAXINFO minMaxInfo;
-			minMaxInfo = (LPMINMAXINFO)lParam;
-
-			RECT rct;
-
-			rct.left = 0;
-			rct.top = 0;
-			rct.right = 640;
-			rct.bottom = 200;
-
-			AdjustWindowRectEx(&rct, WS_OVERLAPPEDWINDOW, FALSE, WS_OVERLAPPEDWINDOW);
-
-			POINT minSize;
-			minSize.x = rct.right - rct.left;
-			minSize.y = rct.bottom - rct.top;
-
-			minMaxInfo->ptMinTrackSize = minSize;
-
-			
-		}
-		break;
+	
 
 	case WM_PAINT:
 		{
@@ -265,9 +243,329 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		}
 		break;
 	
+	case WM_VSCROLL:
+        if (!lParam) 
+        {
+            sbInfo.cbSize = sizeof(SCROLLINFO);
+            sbInfo.fMask = SIF_POS | SIF_RANGE | SIF_PAGE;
+
+            GetScrollInfo(hWnd, SB_VERT, &sbInfo);
+
+            int currScrollY = sbInfo.nPos;
+            int maxLimit = sbInfo.nMax - sbInfo.nPage + 1;
+
+            switch (LOWORD(wParam))
+            {
+            case SB_LEFT:
+                currScrollY = 0;
+                break;
+
+            case SB_RIGHT:
+                currScrollY = maxLimit;
+                break;
+
+            case SB_LINELEFT:                       
+                currScrollY = max(0, sbInfo.nPos-5);
+                break;
+
+            case SB_PAGELEFT:
+                {
+                    RECT rct;
+                    GetClientRect(hWnd, &rct);
+                    currScrollY = max(0, sbInfo.nPos - (rct.bottom -rct.top));
+                }               
+                break;
+
+            case SB_LINERIGHT:
+                currScrollY = min(maxLimit, sbInfo.nPos+5);
+                break;                  
+
+            case SB_PAGERIGHT:
+                {
+                    RECT rct;
+                    GetClientRect(hWnd, &rct);
+                    currScrollY = min(maxLimit, sbInfo.nPos + (rct.bottom -rct.top));
+                }               
+                break;
+
+            case SB_THUMBTRACK:
+            case SB_THUMBPOSITION:
+                {                   
+                    currScrollY = HIWORD(wParam);
+                }
+                break;
+
+            default:
+                break;
+            }
+
+            ScrollWindow(hWnd, 0, prevScrollY - currScrollY, NULL, NULL);
+            prevScrollY = currScrollY;
+            sbInfo.nPos = prevScrollY;
+            SetScrollInfo(hWnd, SB_VERT, &sbInfo, TRUE);
+        }
+		else
+		{
+			case ID_SCROLLHEIGHT:
+				{
+					SCROLLINFO si;
+					si.cbSize = sizeof(SCROLLINFO);
+					si.fMask = SIF_POS | SIF_RANGE;
+
+					GetScrollInfo((HWND)lParam, SB_CTL, &si);
+
+					switch (LOWORD(wParam))
+					{
+					case SB_TOP:
+						si.nPos = si.nMin;
+						break;
+
+					case SB_BOTTOM:
+						si.nPos = si.nMax;
+						break;
+
+					case SB_LINEUP:                       
+						si.nPos = max(si.nMin, si.nPos-5);
+						break;
+
+					case SB_PAGEUP:
+						si.nPos = max(si.nMin, si.nPos-50);
+						break;
+
+					case SB_LINEDOWN:
+						si.nPos = min(si.nMax, si.nPos+5);
+						break;                  
+
+					case SB_PAGEDOWN:
+						si.nPos = min(si.nMax, si.nPos+50);
+						break;
+
+					case SB_THUMBTRACK:     // fall through
+					case SB_THUMBPOSITION:
+						si.nPos = (int)HIWORD(wParam);
+						break;
+
+					default:
+						break;
+					}
+
+					si.fMask = SIF_POS;
+					SetScrollInfo((HWND)lParam, SB_CTL, &si, TRUE);
+
+					RECT rct;
+					rct.left = 0;
+					rct.top = 0;
+					rct.right = SendMessage(GetDlgItem(hWnd, ID_SCROLLWIDTH), SBM_GETPOS, 0, 0);
+					rct.bottom = SendMessage(GetDlgItem(hWnd, ID_SCROLLHEIGHT), SBM_GETPOS, 0, 0);
+
+					AdjustWindowRectEx(&rct, WS_OVERLAPPEDWINDOW, TRUE, WS_EX_OVERLAPPEDWINDOW);
+
+					SetWindowPos(hWnd, HWND_TOP, 0, 0, 
+								 rct.right - rct.left, 
+								 rct.bottom - rct.top, 
+								 SWP_NOMOVE | SWP_NOREPOSITION);
+				}       
+				break;
+		}
+        break;
+
+		case WM_HSCROLL:
+        if (lParam)
+        {
+            int scrollId = GetDlgCtrlID((HWND)lParam);
+
+            switch (scrollId)
+            {
+			case ID_SCROLLSTATIC:
+                {
+                    sbInfo.cbSize = sizeof(SCROLLINFO);
+                    sbInfo.fMask = SIF_POS | SIF_RANGE;
+
+                    GetScrollInfo((HWND)lParam, SB_CTL, &sbInfo);
+
+                    switch (LOWORD(wParam))
+                    {
+                    case SB_LEFT:
+                        sbInfo.nPos = 0;
+                        break;
+
+                    case SB_RIGHT:
+                        sbInfo.nPos = 255;
+                        break;
+
+                    case SB_LINELEFT:                       
+                        sbInfo.nPos = max(sbInfo.nMin, sbInfo.nPos-5);
+                        break;
+
+                    case SB_PAGELEFT:
+                        sbInfo.nPos = max(sbInfo.nMin, sbInfo.nPos-20);
+                        break;
+
+                    case SB_LINERIGHT:
+                        sbInfo.nPos = min(sbInfo.nMax, sbInfo.nPos+5);
+                        break;                  
+
+                    case SB_PAGERIGHT:
+                        sbInfo.nPos = min(sbInfo.nMax, sbInfo.nPos+20);
+                        break;
+
+                    case SB_THUMBTRACK:     
+                    case SB_THUMBPOSITION:
+                        sbInfo.nPos = (int)HIWORD(wParam);
+                        break;
+
+                    default:
+                        break;
+                    }
+
+					txtColor = sbInfo.nPos;
+					InvalidateRect(hStatic, NULL, NULL);
+					sbInfo.fMask = SIF_POS;
+                    SetScrollInfo((HWND)lParam, SB_CTL, &sbInfo, TRUE);
+                }               
+                break;
+
+			
+				
+			case ID_SCROLLWIDTH:
+				{
+					SCROLLINFO si;
+					si.cbSize = sizeof(SCROLLINFO);
+					si.fMask = SIF_POS | SIF_RANGE;
+
+					GetScrollInfo((HWND)lParam, SB_CTL, &si);
+
+					switch (LOWORD(wParam))
+					{
+					case SB_LEFT:
+						si.nPos = si.nMin;
+						break;
+
+					case SB_RIGHT:
+						si.nPos = si.nMax;
+						break;
+
+					case SB_LINELEFT:                       
+						si.nPos = max(si.nMin, si.nPos-5);
+						break;
+
+					case SB_PAGELEFT:
+						si.nPos = max(si.nMin, si.nPos-50);
+						break;
+
+					case SB_LINERIGHT:
+						si.nPos = min(si.nMax, si.nPos+5);
+						break;                  
+
+					case SB_PAGERIGHT:
+						si.nPos = min(si.nMax, si.nPos+50);
+						break;
+
+					case SB_THUMBTRACK:     // fall through
+					case SB_THUMBPOSITION:
+						si.nPos = (int)HIWORD(wParam);
+						break;
+
+					default:
+						break;
+					}
+
+					si.fMask = SIF_POS;
+					SetScrollInfo((HWND)lParam, SB_CTL, &si, TRUE);
+
+					RECT rct;
+					GetClientRect(hWnd, &rct);
+
+					rct.left = 0;
+					rct.top = 0;
+					rct.right = SendMessage(GetDlgItem(hWnd, ID_SCROLLWIDTH), SBM_GETPOS, 0, 0);
+					rct.bottom = SendMessage(GetDlgItem(hWnd, ID_SCROLLHEIGHT), SBM_GETPOS, 0, 0);
+
+					AdjustWindowRect(&rct, WS_OVERLAPPEDWINDOW, TRUE);
+
+					SetWindowPos(hWnd, HWND_TOP, 0, 0, 
+								 rct.right - rct.left, 
+								 rct.bottom - rct.top, 
+								 SWP_NOMOVE | SWP_NOREPOSITION);
+				}        
+				break;
+
+
+            default:
+                break;
+            }           
+        }
+        else
+        {
+            sbInfo.cbSize = sizeof(SCROLLINFO);
+            sbInfo.fMask = SIF_POS | SIF_RANGE | SIF_PAGE;
+
+            GetScrollInfo(hWnd, SB_HORZ, &sbInfo);
+
+            int currScrollX = sbInfo.nPos;
+            int maxLimit = sbInfo.nMax - sbInfo.nPage + 1;
+
+            switch (LOWORD(wParam))
+            {
+            case SB_LEFT:
+                currScrollX = 0;
+                break;
+
+            case SB_RIGHT:
+                currScrollX = maxLimit;
+                break;
+
+            case SB_LINELEFT:                       
+                currScrollX = max(0, sbInfo.nPos-5);
+                break;
+
+            case SB_PAGELEFT:
+                {
+                    RECT rct;
+                    GetClientRect(hWnd, &rct);
+                    currScrollX = max(0, sbInfo.nPos - (rct.right -rct.left));
+                }               
+                break;
+
+            case SB_LINERIGHT:
+                currScrollX = min(maxLimit, sbInfo.nPos+5);
+                break;                  
+
+            case SB_PAGERIGHT:
+                {
+                    RECT rct;
+                    GetClientRect(hWnd, &rct);
+                    currScrollX = min(maxLimit, sbInfo.nPos + (rct.right -rct.left));
+                }               
+                break;
+
+            case SB_THUMBTRACK:
+            case SB_THUMBPOSITION:
+                {                   
+                    currScrollX = HIWORD(wParam);
+                }
+                break;
+
+            default:
+                break;
+            }
+
+            ScrollWindow(hWnd, prevScrollX - currScrollX, 0, NULL, NULL);
+            prevScrollX = currScrollX;
+            sbInfo.nPos = prevScrollX;
+            SetScrollInfo(hWnd, SB_HORZ, &sbInfo, TRUE);
+        }       
+        break;
+
+	case WM_CTLCOLORSTATIC:
+		{
+
+			SetBkMode((HDC)wParam,TRANSPARENT);
+			SetTextColor((HDC)wParam, RGB(txtColor, txtColor, txtColor));
+		}
+		break;
 	
-		
-	
+
 
 	case WM_DESTROY:
 		UnregisterHotKey(hWnd, HK_CLOSE);
